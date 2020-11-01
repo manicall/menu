4997bd90f50ade2083db039b73d6ebc43c4b702a
max <makskms1@gmail.com>
2020-11-01 12:48:59 +1000

    иконка на панели задач

--- experement.py
+++ experement.py
@@ -1,70 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-- Щелкните мышью на надписи, чтобы установить фокус ввода
-- Нажмите любую клавишу
-- Нажмите обычную клавишу вместе с модификаторами
-- Нажмите комбинацию клавиш <Ctrl>+<C>
-- Нажмите клавишу <B>
-"""
-from PyQt5 import QtCore, QtWidgets, QtGui
+#!/usr/bin/env python3
 
+import os
+import sys
+import subprocess
+import os.path
 
-class MyLabel(QtWidgets.QLabel):
-    def __init__(self, text, parent=None):
-        QtWidgets.QLabel.__init__(self, text, parent)
-        self.setFocusPolicy(QtCore.Qt.StrongFocus)
-        self.setAlignment(QtCore.Qt.AlignCenter)
+from PyQt4 import QtGui
+from PyQt4 import QtCore
 
-    def focusInEvent(self, e):
-        self.grabKeyboard()
-        QtWidgets.QLabel.focusInEvent(self, e)
-
-    def focusOutEvent(self, e):
-        self.releaseKeyboard()
-        QtWidgets.QLabel.focusOutEvent(self, e)
-
-    def keyPressEvent(self, e):
-        msg, modifiers = [], []
-        if e.modifiers() & QtCore.Qt.ShiftModifier:
-            modifiers.append("Shift")
-        if e.modifiers() & QtCore.Qt.ControlModifier:
-            if e.key() == QtCore.Qt.Key_B:
-                print('c')
-        if e.modifiers() & QtCore.Qt.AltModifier:
-            modifiers.append("Alt")
-        if len(modifiers) == 0:
-            modifiers.append("No")
-        if e.matches(QtGui.QKeySequence.Copy):
-            msg.append("Нажата комбинация <Ctrl>+<C>")
-        if e.key() == QtCore.Qt.Key_B:
-            msg.append("Нажата клавиша <B>")
-        self.setText(
-            "Код: {0}, символ: {1}\nmodifiers: {2}\n{3}".format(
-                e.key(), e.text(), "+".join(modifiers), "\n".join(msg)))
-        e.ignore()
-        QtWidgets.QLabel.keyPressEvent(self, e)
-
-    def keyReleaseEvent(self, e):
-        print("Клавиша отпущена")
-        QtWidgets.QLabel.keyReleaseEvent(self, e)
-
-
-class MyWindow(QtWidgets.QWidget):
+class MyWin(QtGui.QMainWindow):
     def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.resize(300, 150)
-        self.label = MyLabel("Нажмите любую клавишу")
-        self.label.setFrameStyle(QtWidgets.QFrame.Box |
-                                 QtWidgets.QFrame.Plain)
-        self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.addWidget(self.label)
-        self.setLayout(self.vbox)
+        super(MyWin, self).__init__(parent)
+        self.setWindowTitle("My Window")
+        self.setWindowIcon(QtGui.QIcon('test_icon.png'))
+        self.show()
 
+def main(args):
+    app = QtGui.QApplication([])
 
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
+    ww= MyWin()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- main.pyw
+++ main.pyw
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 from modules.mainwindow import MainWindow
 import time
 import re
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+# нам понадобится модуль winextras
+import sys
+
+try:
+    # Включите в блок try/except, если вы также нацелены на Mac/Linux
+    from PyQt5.QtWinExtras import QtWin                                         #  !!!
+    myappid = 'mycompany.myproduct.subproduct.version'                          #  !!!
+    QtWin.setCurrentProcessExplicitAppUserModelID(myappid)                      #  !!!
+except ImportError:
+    pass
+
 start_time = time.time()
-
-
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle(QtWidgets.QStyleFactory.create('Fusion'))
+    app.setWindowIcon(QtGui.QIcon('images\program_icon.png'))
     desktop = QtWidgets.QApplication.desktop().availableGeometry()
     window = MainWindow()
     window.move(0, 0)
     window.resize(int(desktop.width()) - 100, int(QtWidgets.QApplication.desktop().height() * (9/16)))
-
     # открытие файла
     p = re.compile(r".\.svdfl")
     if len(sys.argv) > 1:
@@ -27,5 +38,6 @@ if __name__ == '__main__':
         window.open_early_file()
     window.show()
     print("--- %s seconds ---" % (time.time() - start_time))
+
     window_result = app.exec()
     sys.exit(window_result)
